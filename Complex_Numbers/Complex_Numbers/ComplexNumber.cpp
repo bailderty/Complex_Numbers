31e425fa78ea2db30fcb0988bafcdd8842cfe798
bailderty <brettmeyer@Bretts-Mac-mini-6.local>
2016-04-22 09:14:49 -0700

    Added Needed files

---
+++ Complex_Numbers/Complex_Numbers/ComplexNumber.cpp
@@ -0,0 +1,9 @@
+//
+//  ComplexNumber.cpp
+//  Complex_Numbers
+//
+//  Created by Brett Meyer on 4/21/16.
+//  Copyright © 2016 Brett Meyer. All rights reserved.
+//
+
+#include "ComplexNumber.h"
---
+++ Complex_Numbers/Complex_Numbers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Complex_Numbers
+//
+//  Created by Brett Meyer on 4/21/16.
+//  Copyright © 2016 Brett Meyer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
